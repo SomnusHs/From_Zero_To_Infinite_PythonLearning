f49cfad482a38116778d9712524c71f2c94fcac2
Renng Hua <2498224075@qq.com>
2026-04-12 00:34:40 +0800

    2026.04.11_PythonCodeLearning & DailyQuestions
     DAY05

---
+++ Day05.py
@@ -0,0 +1,131 @@
+### 使用if和else构造分支结构
+
+# Example 1：BMI计算器 Version 1
+
+height = float(input('身高(cm)：'))
+weight = float(input('体重(kg)：'))
+bmi = weight / (height / 100) ** 2
+print(f'{bmi = :.1f}')
+if 18.5 <= bmi < 24:
+    print('你的身材很棒！')
+
+# Example 2：BMI计算器 Version 2
+
+height = float(input('身高(cm)：'))
+weight = float(input('体重(kg)：'))
+bmi = weight / (height / 100) ** 2
+print(f'{bmi = :.1f}')
+if 18.5 <= bmi < 24:
+    print('你的身材很棒！')
+else:
+    print('你的身材不够标准哟！')
+
+# Example 3：BMI计算器 Version 3
+
+height = float(input('身高(cm)：'))
+weight = float(input('体重(kg)：'))
+bmi = weight / (height / 100) ** 2
+print(f'{bmi = :.1f}')
+if bmi < 18.5:
+    print('你的体重过轻！')
+elif bmi < 24:
+    print('你的身材很棒！')
+elif bmi < 27:
+    print('你的体重过重！')
+elif bmi < 30:
+    print('你已轻度肥胖！')
+elif bmi < 35:
+    print('你已中度肥胖！')
+else:
+    print('你已重度肥胖！')
+
+
+
+### 使用match和case构造分支结构
+
+# Example 4：BMI计算器 Version 4
+
+status_code = int(input('响应状态码: '))
+if status_code == 400:
+    description = 'Bad Request'
+elif status_code == 401:
+    description = 'Unauthorized'
+elif status_code == 403:
+    description = 'Forbidden'
+elif status_code == 404:
+    description = 'Not Found'
+elif status_code == 405:
+    description = 'Method Not Allowed'
+elif status_code == 418:
+    description = 'I am a teapot'
+elif status_code == 429:
+    description = 'Too many requests'
+else:
+    description = 'Unknown status Code'
+print('状态码描述:', description)
+
+# Example 5：状态响应码 Version 1
+
+status_code = int(input('响应状态码: '))
+match status_code:
+    case 400: description = 'Bad Request'
+    case 401: description = 'Unauthorized'
+    case 403: description = 'Forbidden'
+    case 404: description = 'Not Found'
+    case 405: description = 'Method Not Allowed'
+    case 418: description = 'I am a teapot'
+    case 429: description = 'Too many requests'
+    case _: description = 'Unknown Status Code'
+print('状态码描述:', description)
+
+# Example 6：状态响应码 Version 2
+
+status_code = int(input('响应状态码: '))
+match status_code:
+    case 400 | 405: description = 'Invalid Request'
+    case 401 | 403 | 404: description = 'Not Allowed'
+    case 418: description = 'I am a teapot'
+    case 429: description = 'Too many requests'
+    case _: description = 'Unknown Status Code'
+print('状态码描述:', description)
+
+
+
+### 分支结构的描述
+
+# Example 1：分段函数求值
+
+x = float(input('x = '))
+if x > 1:
+    y = 3 * x - 5
+elif x >= -1:
+    y = x + 2
+else:
+    y = 5 * x + 3
+print(f'{y = }')
+
+# Example 2：分段函数求值（嵌套if）
+
+x = float(input('x = '))
+if x > 1:
+    y = 3 * x - 5
+else:
+    if x >= -1:
+        y = x + 2
+    else:
+        y = 5 * x + 3
+print(f'{y = }')
+
+# Example 3：计算三角形的周长和面积
+
+a = float(input('a = '))
+b = float(input('b = '))
+c = float(input('c = '))
+if a + b > c and a + c > b and b + c > a:
+    perimeter = a + b + c
+    print(f'周长: {perimeter}')
+    s = perimeter / 2
+    area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+    print(f'面积: {area}')
+else:
+    print('不能构成三角形')
